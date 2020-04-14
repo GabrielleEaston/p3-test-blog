d46f8ea09ef1c9ee43a64f064069e89df561e05f
Gabrielle <eastongabrielle@gmail.com>
2020-04-14 19:55:02 -0400

    finished deployment

---
+++ client/build/precache-manifest.835e9ca9f7c015b126dc1ca9fb5dcb57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d59ef8908685591803a27cbfa38721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf95724fb2d15176bbe",
+    "url": "/static/css/main.f24397bd.chunk.css"
+  },
+  {
+    "revision": "c72dc0809f797fd323de",
+    "url": "/static/js/2.d2a1a49b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d2a1a49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf95724fb2d15176bbe",
+    "url": "/static/js/main.238e0de3.chunk.js"
+  },
+  {
+    "revision": "a15422c73947459aac30",
+    "url": "/static/js/runtime-main.2b4c64c5.js"
+  }
+]);
